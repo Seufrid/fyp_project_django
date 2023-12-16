21e7b5bf116c59db968a9458923f509b4deeda98
Seufrid <usamaabdul11@gmail.com>
2023-12-16 20:19:48 +0800

    selftest form working

---
+++ web_project/static/js/selftest.js
@@ -0,0 +1,22 @@
+// Using Fetch API to send data to the view then display the result in the HTML page
+document.addEventListener('DOMContentLoaded', function() {
+    var form = document.querySelector('#myForm');
+    var url = form.dataset.url;
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        var formData = new FormData(this);
+
+        fetch(url, {
+            method: 'POST',
+            body: formData
+        })
+        .then(response => response.json())
+        .then(data => {
+            document.querySelector('#result').textContent = '';
+            var percentage = (data.result * 100).toFixed(0);
+            document.querySelector('#result').textContent += "Chance of heart failure = " + percentage + '%';
+        });
+    });
+});
--- web_project/views.py
+++ web_project/views.py
@@ -1,16 +1,6 @@
-from django.http import HttpResponse
+import pickle
 from django.shortcuts import render
-
-data = {
-    "reasons_to_learn": [
-        {"reason": "Clean design"},
-        {"reason": "Python power"},
-        {"reason": "Community"},
-        {"reason": "Django is fun"},
-        {"reason": "Job market"},
-    ],
-}
-
+from django.http import JsonResponse
 
 def home(request):
     return render(request, "home.html")
@@ -22,7 +12,29 @@ def contact(request):
     return render(request, "contact.html")
 
 def selftest(request):
-    return render(request, "selftest.html")
+    if request.method == 'POST':
+        age = int(request.POST['Age'])
+        sex = int(request.POST['Sex'])
+        chest_pain_type = int(request.POST['ChestPainType'])
+        resting_bp = int(request.POST['RestingBP'])
+        cholesterol = int(request.POST['Cholesterol'])
+        fasting_bs = int(request.POST['FastingBS'])
+        resting_ecg = int(request.POST['RestingECG'])
+        max_hr = int(request.POST['MaxHR'])
+        exercise_angina = int(request.POST['ExerciseAngina'])
+        oldpeak = int(request.POST['Oldpeak'])
+        st_slope = int(request.POST['STSlope'])
 
+        model = pickle.load(open("heart_model.sav", "rb"))
+        prediction = model.predict_proba([[age, sex, chest_pain_type, resting_bp, cholesterol, fasting_bs, resting_ecg, max_hr, exercise_angina, oldpeak, st_slope]])
+        result = prediction[0][1]
+
+        return JsonResponse({'result': result})
+    else:
+        return render(request, "selftest.html")
+    
 def appointment(request):
     return render(request, "appointment.html")
+
+def test(request):
+    return render(request, "test.html")
